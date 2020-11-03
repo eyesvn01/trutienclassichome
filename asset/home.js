e56d7555b7c29b707a49869056b5f14f129048f9
Trung Anh <eyesv@DESKTOP-C079A1S>
2020-11-03 14:14:32 +0700

    init

---
+++ asset/home.js
@@ -0,0 +1,362 @@
+var role = [
+    {
+        img: "./asset/img-ph.png",
+        name: "PHẦN HƯƠNG",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-ph.png",
+    },
+    {
+        img: "/asset/img-tv.png",
+        name: "THANH VÂN",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-tv.png",
+    },
+    {
+        img: "./asset/img-qd.png",
+        name: "QUỶ ĐẠO",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-qd.png",
+    },
+    {
+        img: "./asset/img-ta.png",
+        name: "THIÊN ÂM",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                    tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    consequat.
+                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+                    velillum dolore eu.`,
+        infor: "./asset/inf-ta.png",
+    },
+    {
+        img: "./asset/img-qv.png",
+        name: "QUỶ VƯƠNG",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-qv.png",
+    },
+    {
+        img: "./asset/img-hh.png",
+        name: "HỢP HOAN",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-hh.png",
+    },
+    {
+        img: "./asset/img-thoang.png",
+        name: "THẦN HOÀNG",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-thoang.png",
+    },
+    {
+        img: "./asset/img-ls.png",
+        name: "LIỆT SƠN",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-ls.png",
+    },
+    {
+        img: "./asset/img-thoa.png",
+        name: "THIÊN HOA",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-thoa.png",
+    },
+    {
+        img: "./asset/img-cl.png",
+        name: "CỬU LÊ",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                    tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    consequat.
+                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+                    velillum dolore eu.`,
+        infor: "./asset/inf-cl.png",
+    },
+    {
+        img: "./asset/img-th.png",
+        name: "THÁI HẠO",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-th.png",
+    },
+    {
+        img: "./asset/img-hq.png",
+        name: "HOÀI QUANG",
+        desc: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod 
+            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo 
+            consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            velillum dolore eu.`,
+        infor: "./asset/inf-hq.png",
+    },
+];
+
+let currentMainHover;
+
+function reset() {
+    for (var i = 1; i <= 2; i++) {
+        $(`.child[menu${i}]`).each(function () {
+            $(this).removeClass("active");
+        });
+
+        $(`inner[menu${i}]`).each(function () {
+            $(this).addClass("hide");
+        });
+    }
+}
+
+function hoverMain(idx) {
+    currentMainHover = idx;
+    reset();
+    $(`.child[menu${idx}]`).each(function () {
+        $(this).addClass("active");
+    });
+
+    $(`inner[menu${idx}]`).each(function () {
+        $(this).removeClass("hide");
+    });
+}
+function hoverOut(idx) { }
+$(() => {
+    $(".child").on("mouseover", function () { });
+});
+
+$(".child").on("click", function () {
+    var idx = $(this).data("idx");
+    if ($(this).find("inner[menu2]").not(".hide").length) {
+        idx += 3;
+    }
+    $("#role_img").attr("src", role[idx].img);
+    $(".role_name").html(role[idx].name);
+    $(".role_desc").html(role[idx].desc);
+    $(".role_info img").attr("src", role[idx].infor);
+});
+
+function clickChild(idx) {
+    $("#role_img").attr("src", role[idx].img);
+    $(".role_name").html(role[idx].name);
+    $(".role_desc").html(role[idx].desc);
+    $(".role_info img").attr("src", role[idx].infor);
+}
+
+$(function () {
+    lunboFocus("#slide_btn p", "#slide_item td", 4, ".slide_img .lb_cont", 0, 4);
+});
+function lunboFocus(
+    lb_btn,
+    lb_img_btn_dom,
+    lb_img_btn_num,
+    lb_dd,
+    btn_int,
+    speed
+) {
+    if (!lb_btn || !lb_img_btn_dom || !lb_dd) return;
+    var lb_dd = $(lb_dd);
+    var lb_btn = $(lb_btn);
+    var lb_img_btn_dom = lb_img_btn_dom;
+    var lb_img_btn = $(lb_img_btn_dom);
+    if (!btn_int) {
+        btn_int = 0;
+    }
+    var lb_img_btn_width = lb_img_btn.width() + btn_int;
+    var lb_img_box = $("#shop_item");
+    lb_img_box.width(lb_img_btn_width * lb_img_btn_num);
+    if (!speed) {
+        speed = 5000;
+    } else {
+        speed *= 1000;
+    }
+    var re_auto = lb_img_btn.length * (lb_img_btn.width() + btn_int);
+    var alpha_img = 1;
+    var alpha_img_curr = 1;
+    var lb_auto = setInterval(lb_next, speed);
+    lb_img_btn.first().add(lb_dd.first()).addClass("curr first");
+    lb_img_btn.last().add(lb_dd.last()).addClass("last");
+    lb_img_btn
+        .css("marginRight", btn_int + "px")
+        .fadeTo("fast", alpha_img)
+        .siblings(".curr")
+        .fadeTo("fast", alpha_img_curr);
+    lb_dd.filter(".curr").fadeTo("fast", alpha_img_curr);
+    var dizeng = lb_img_btn.width() + btn_int;
+    lb_img_btn.each(function (i) {
+        $(this).css("left", btn_int);
+        btn_int += dizeng;
+    });
+    lb_img_btn
+        .hover(
+            function () {
+                $(this).stop($(this).fadeIn()).fadeTo("fast", alpha_img_curr);
+            },
+            function () {
+                if (!$(this).is(".curr")) {
+                    $(this).fadeTo("normal", alpha_img);
+                }
+            }
+        )
+        .click(function () {
+            $(this).siblings(".curr").removeClass("curr").fadeTo("normal", alpha_img);
+            $(this).addClass("curr").fadeTo("normal", alpha_img_curr);
+            var n = $(this).index(lb_img_btn_dom);
+            lb_dd.filter(".curr").removeClass("curr").hide();
+            lb_dd.eq(n).addClass("curr").fadeTo("slow", alpha_img_curr);
+        });
+    lb_btn.first().click(function () {
+        lb_prev();
+    });
+    lb_btn.last().click(function () {
+        lb_next();
+    });
+    function outview(direction) {
+        var currpos1 = lb_img_box.scrollLeft() - lb_img_btn.outerWidth(true);
+        var currpos2 = lb_img_box.scrollLeft() + lb_img_box.width();
+        var nnn = lb_img_btn.filter(".curr").index(lb_img_btn_dom);
+        if (
+            !(nnn * lb_img_btn_width > currpos1 && nnn * lb_img_btn_width < currpos2)
+        ) {
+            if (direction) {
+                lb_img_box.animate(
+                    { scrollLeft: lb_img_box.scrollLeft() + lb_img_box.width() },
+                    300
+                );
+            } else {
+                lb_img_box.animate(
+                    { scrollLeft: lb_img_box.scrollLeft() - lb_img_box.width() },
+                    300
+                );
+            }
+        }
+    }
+    function lb_prev() {
+        if (lb_img_btn.filter(".curr").is(".first")) {
+            lb_img_btn
+                .last()
+                .addClass("curr")
+                .fadeTo("slow", alpha_img_curr)
+                .end()
+                .filter(".curr:first")
+                .removeClass("curr")
+                .fadeTo("slow", alpha_img);
+            lb_dd
+                .last()
+                .addClass("curr")
+                .fadeIn("slow")
+                .end()
+                .filter(".curr:first")
+                .removeClass("curr")
+                .fadeOut("slow");
+            lb_img_box
+                .animate(
+                    {
+                        scrollLeft:
+                            lb_img_btn.outerWidth(true) *
+                            lb_img_btn.last().index(lb_img_btn_dom),
+                    },
+                    re_auto
+                )
+                .scrollLeft(
+                    lb_img_btn.outerWidth(true) * lb_img_btn.last().index(lb_img_btn_dom)
+                );
+        } else {
+            lb_img_btn
+                .filter(".curr")
+                .prev()
+                .addClass("curr")
+                .fadeTo("slow", alpha_img_curr)
+                .end()
+                .removeClass("curr")
+                .fadeTo("slow", alpha_img);
+            lb_dd
+                .filter(".curr")
+                .prev()
+                .addClass("curr")
+                .fadeIn("slow")
+                .end()
+                .removeClass("curr")
+                .fadeOut("slow");
+            outview(false);
+        }
+    }
+    function lb_next() {
+        if (!lb_dd) return;
+        if (lb_img_btn.filter(".curr").is(".last")) {
+            lb_img_btn
+                .first()
+                .addClass("curr")
+                .fadeTo("slow", alpha_img_curr)
+                .end()
+                .filter(".curr:last")
+                .removeClass("curr")
+                .fadeTo("slow", alpha_img);
+            lb_dd
+                .first()
+                .addClass("curr")
+                .fadeIn("slow")
+                .end()
+                .filter(".curr:last")
+                .removeClass("curr")
+                .fadeOut("slow");
+            lb_img_box.animate({ scrollLeft: 0 }, re_auto);
+        } else {
+            lb_img_btn
+                .filter(".curr")
+                .next()
+                .addClass("curr")
+                .fadeTo("slow", alpha_img_curr)
+                .end()
+                .removeClass("curr")
+                .fadeTo("slow", alpha_img);
+            lb_dd
+                .filter(".curr")
+                .next()
+                .addClass("curr")
+                .fadeIn("slow")
+                .end()
+                .removeClass("curr")
+                .fadeOut("slow");
+            outview(true);
+        }
+    }
+}
